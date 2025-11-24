b1729dfbaf9efac2e962111437117ff80a612577
Osbert Kamugisha <kamugishaosbert7@gmail.com>
2025-11-24 19:58:37 +0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter BrowserRouter="/uhsms">
+        <BrowserRouter basename="/uhsms">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
